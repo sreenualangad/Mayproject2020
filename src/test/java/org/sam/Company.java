09bb14e043c0e9f1207c916d30540e20d2782834
sreenualangad <sreenivasanalangad@gmail.com>
2020-05-05 09:50:36 +0530

    2nd msg emp2

--- src/test/java/org/sam/Company.java
+++ src/test/java/org/sam/Company.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
 	System.out.println("Employee1 job done");
 	
 	System.out.println("Employee2 job done");
+	
+	System.out.println("employee2 seconed job");
 }
 }
